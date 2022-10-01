b1b49d2b88a4a984842478fc35185190c3a3dcfa
Mariia-Kovalenko <64276716+Mariia-Kovalenko@users.noreply.github.com>
2022-10-01 16:06:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# draggable-part-6-wunpko
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/draggable-part-6-wunpko)
---
+++ app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+function remove(item: string, list: string[]) {
+  if (list.indexOf(item) !== -1) {
+    list.splice(list.indexOf(item), 1);
+  }
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  availableBoxes = ['Box 1', 'Box 2', 'Box 3', 'Box 4', 'Box 5'];
+
+  dropzone1 = ['Box 6'];
+
+  dropzone2 = ['Box 7'];
+
+  dropzone3 = [];
+
+  currentBox?: string;
+
+  move(box: string, toList: string[]): void {
+    remove(box, this.availableBoxes);
+    remove(box, this.dropzone1);
+    remove(box, this.dropzone2);
+    remove(box, this.dropzone3);
+
+    toList.push(box);
+  }
+}
